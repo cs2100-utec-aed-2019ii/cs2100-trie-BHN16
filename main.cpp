d7f507396600a3fe911f2a8af09fcac37fdd8ff8
BHN16 <34187868+BHN16@users.noreply.github.com>
2019-11-17 18:26:46 -0500

    Feature<main.cpp> : All the methods work fine.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,11 @@ int main() {
     U.insert("foo");
     U.insert("fifo");
     std::cout << "Se encontro : " << U.search_by_complete("fif") << std::endl;
-
+    U.erase("fa");
+    U.erase("fifo");
+    std::cout << "Se encontro : " << U.search_by_complete("fa") << std::endl;
+    std::cout << "Se encontro : " << U.search_by_complete("fifo") << std::endl;
+    U.insert("hola");
+    U.print();
     return 0;
 }
